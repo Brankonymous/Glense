e8f38685935641bc90b327c5f7b227519d95b0df
kerkoc01 <stefan.kerkoc@gmail.com>
2024-12-03 03:50:45 +0100

    Setting up database connection

---
+++ Glense.Server/Comment.cs
@@ -0,0 +1,19 @@
+﻿namespace Glense.Server
+{
+    public class Comment
+    {
+        public int commentId {  get; set; }
+        public Video video { get; set; }
+        public int videoId { get; set; }
+        public User user { get; set; }
+        public int userId { get; set; }
+        public string commentText { get; set; }
+        public DateTime createdAt { get; set; }
+        public Comment parentComment { get; set; }
+        public int parentCommentId { get; set; }
+        public int commentLikes { get; set; }
+        public ICollection<Comment> ChildComments { get; set; }
+        public ICollection<CommentLikes> CLikes { get; set; }
+
+    }
+}
